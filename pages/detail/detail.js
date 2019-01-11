d40741dad3a5eeba37c94559fd730cd63d5247fb
zhiyongzaixian <13852292775@163.com>
2019-01-11 16:59:05 +0800

    'xx'

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    detailObj: {}
+    detailObj: {},
+    isCollected: false // 标识文章是否被收藏
   },
 
   /**
@@ -19,8 +20,55 @@ Page({
     let index = options.index;
     // 更新detailObj的状态
     this.setData({
-      detailObj: dataArr[index]
+      detailObj: dataArr[index],
+      index
     })
+
+    // 获取本地缓存的是否收藏的状态值
+    let oldStorage = wx.getStorageSync('isCollected');
+    if (oldStorage[index]){
+      // 更新isCollected的状态值
+      this.setData({
+        isCollected: true
+      })
+    }
+  },
+  handleCollection(){
+    console.log('x');
+    // 更新isCollected的状态
+    let isCollected = !this.data.isCollected;
+    this.setData({
+      isCollected: isCollected
+    })
+
+    // 显示提示信息
+    let title = isCollected?'收藏成功': '取消收藏';
+    wx.showToast({
+      title,
+      icon: 'success'
+    })
+
+
+    // 将是否收藏的状态缓存到本地
+    // 注意： 多个文章页面不要共享一个状态
+    // 理想化： {0: true, 1: false, 2: true}
+    // 实现：对象， 下标
+    let index = this.data.index;
+    // let obj  = {};
+    let obj = wx.getStorageSync('isCollected');
+    console.log(obj, typeof obj);
+    if(!obj){
+      // 之前没有缓存收藏的状态
+      obj = {};
+    }
+    obj[index] = isCollected;
+    
+
+    wx.setStorage({
+      key: 'isCollected',
+      data: obj
+    })
+
   },
 
   /**
